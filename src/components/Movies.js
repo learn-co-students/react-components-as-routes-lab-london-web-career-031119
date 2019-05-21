729a4659028cdcb150e743f5ccb3114152809ca6
Umar--Khan <umarkhandev@gmail.com>
2019-05-21 10:55:17 +0100

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import { movies } from '../data';
+import React from "react";
+import { movies } from "../data";
 
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map((movie, idx) => (
+        <div className="movies" key={idx}>
+          <p>{movie.title}</p>
+          <p>{movie.time}</p>
+          <p>{movie.genres}</p>
+          <p>{movie.metascore}</p>
+        </div>
+      ))}
     </div>
   );
 };
